47603c273a3b6fa8fc39f3edbcf6a1ebba4ef7f9
Matthew L. Roy <mroyucs@outlook.com>
2023-06-12 10:40:59 -0700

    add 2 new sorting algorithms, add comments of prompts to chatgpt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import numpy as np
 from enum import Enum
 import pprint
+import random
 
 startTime = datetime.now()
 print("\nStarted script at: {}".format(startTime))
@@ -13,6 +14,8 @@ print("\n* * * * BEGIN CODE EXECUTION * * * *\n")
 class Sorting_Type(Enum):
     NP_SORT = 1
     CHATGPT_QUICK_SORT = 2
+    CHATGPT_QUICK_SORT_RANDOM_PIVOT = 3
+    CHATGPT_OPTIMAL_SORT = 4
 
 def init_array_i32(N):
     arr = np.random.randint(0, 2**31-1, size=N, dtype=np.int32)
@@ -21,6 +24,7 @@ def init_array_i32(N):
 def np_sort(arr):
     np.sort(arr)
 
+# PROMPT: Create a sorting algorithm for an extremely large array of int32s
 def chatgpt_quick_sort(arr, low, high):
     if low < high:
         pivot = arr[high]
@@ -34,11 +38,57 @@ def chatgpt_quick_sort(arr, low, high):
         chatgpt_quick_sort(arr, low, pi - 1)
         chatgpt_quick_sort(arr, pi + 1, high)
 
+# PROMPT: Enhance your previous (chatgpt_quick_sort) function to be more optimal
+def chatgpt_quick_sort_randomized_pivot(arr, low, high):
+    if low < high:
+        pivot_index = random.randint(low, high)
+        arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+        pivot = arr[high]
+        i = low - 1
+        for j in range(low, high):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        pi = i + 1
+        chatgpt_quick_sort_randomized_pivot(arr, low, pi - 1)
+        chatgpt_quick_sort_randomized_pivot(arr, pi + 1, high)
+
+# PROMPT: The sort you made is still 3 orders of mangitude slower than numpys sort. optimzie it more
+def chatgpt_optimal_sort(arr, low, high):
+    if low < high:
+        # Use Insertion Sort for small arrays
+        if high - low + 1 < 10:
+            for i in range(low + 1, high + 1):
+                key = arr[i]
+                j = i - 1
+                while j >= low and arr[j] > key:
+                    arr[j + 1] = arr[j]
+                    j -= 1
+                arr[j + 1] = key
+            return
+        
+        # Use QuickSort for larger arrays
+        pivot_index = random.randint(low, high)
+        arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+        pivot = arr[high]
+        i = low - 1
+        for j in range(low, high):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        pi = i + 1
+        chatgpt_optimal_sort(arr, low, pi - 1)
+        chatgpt_optimal_sort(arr, pi + 1, high)
+
 orders_of_magnitude = 6
 
 execution_time_dict = {
     Sorting_Type.NP_SORT: {},
     Sorting_Type.CHATGPT_QUICK_SORT: {},
+    Sorting_Type.CHATGPT_QUICK_SORT_RANDOM_PIVOT: {},
+    Sorting_Type.CHATGPT_OPTIMAL_SORT: {},
 }
 
 print(execution_time_dict)
@@ -54,12 +104,26 @@ for i in range(1, orders_of_magnitude + 1):
     endTime = datetime.now()
     execution_time_dict[Sorting_Type.NP_SORT].update({elms: str(endTime - startTime)})
 
-    #chatgpt sort
+    #chatgpt quick sort
     arr = init_array_i32(elms)
     startTime = datetime.now()
     chatgpt_quick_sort(arr, 0, len(arr) - 1)
     endTime = datetime.now()
     execution_time_dict[Sorting_Type.CHATGPT_QUICK_SORT].update({elms: str(endTime - startTime)})
+
+    #chatgpt sort pivot
+    arr = init_array_i32(elms)
+    startTime = datetime.now()
+    chatgpt_quick_sort_randomized_pivot(arr, 0, len(arr) - 1)
+    endTime = datetime.now()
+    execution_time_dict[Sorting_Type.CHATGPT_QUICK_SORT_RANDOM_PIVOT].update({elms: str(endTime - startTime)})
+
+    #chatgpt sort optimal
+    arr = init_array_i32(elms)
+    startTime = datetime.now()
+    chatgpt_optimal_sort(arr, 0, len(arr) - 1)
+    endTime = datetime.now()
+    execution_time_dict[Sorting_Type.CHATGPT_OPTIMAL_SORT].update({elms: str(endTime - startTime)})
 
     #new line formatting
     print()
